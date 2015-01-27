ac19e63f1c1f72a7857184135799f18972e0da72
J.Paul Rinehimer <jprinehimer@gmail.com>
2015-01-27 12:10:27 -0800

    Updates blogroll and social links.

--- pelicanconf.py
+++ pelicanconf.py
@@ -21,13 +21,12 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+		 ('PyHOGS', 'http://pyhogs.github.io'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('Github', 'https://github.com/jprine'),)
+FEEDS = True
+GITHUB_URL = "https://github.com/jprine"
 
 DEFAULT_PAGINATION = False
 
